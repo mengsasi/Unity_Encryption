c3064bf00c590ea5d46129736f4d6c0b52eebc59
mengsasi <mengsasi@outlook.com>
2019-10-07 16:03:58 +0800

    加密

--- AssemblyEncryptionDemo/Assets/Scripts/Test.cs
+++ AssemblyEncryptionDemo/Assets/Scripts/Test.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Test : MonoBehaviour {
 
-
+    private void Start() {
+        var obj = GameObject.CreatePrimitive( PrimitiveType.Cube );
+        obj.transform.position = new Vector3( 0, 0, 0 );
+    }
 
 }
